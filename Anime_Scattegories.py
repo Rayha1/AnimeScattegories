5a3017fc26bcb5c7ebdda4639ddfb2e1f34ca404
Rayha1 <103225542+Rayha1@users.noreply.github.com>
2023-06-05 22:24:27 +1200

    1.4
    added demo

--- Anime_Scattegories.py
+++ Anime_Scattegories.py
@@ -3,7 +3,7 @@ a scategories type thing but it's anime related, for use by the anime club
 first patch made 05/06/23 by Rayha
 
 Author/s: Rayha
-Version: 1.3
+Version: 1.4
 Date updated: 05/06/23
 Extra notes:
 used chatgpt to make parts of this
@@ -18,33 +18,33 @@ import random
 
 # categories
 categories = [
-    "Anime Genres",
-    "Anime Characters",
-    "Anime Studios",
-    "Anime Movies",
-    "Anime Series",
-    "Anime Soundtracks",
-    "Anime Adaptations",
-    "Anime Openings/Endings",
-    "Anime Cosplay",
-    "Anime Conventions",
-    "Anime Streaming Platforms",
-    "Anime Crossovers",
-    "Anime Fandom",
-    "Anime Merchandise",
-    "Anime Voice Actors",
-    "Anime Art Styles",
-    "Anime Directors",
-    "Anime Symbolism",
-    "Anime Mecha",
-    "Anime Romance",
-    "Anime Fantasy Worlds",
-    "Anime Slice of Life",
-    "Anime Strong Female Characters",
-    "Anime Antiheroes",
-    "Anime Villains",
-    "Anime Comedy",
-    "Anime Fan Art"
+    "Genres",
+    "Characters",
+    "Studios",
+    "Movies",
+    "Series",
+    "Soundtracks",
+    "Adaptations",
+    "Openings/Endings",
+    "Cosplay",
+    "Conventions",
+    "Streaming Platforms",
+    "Crossovers",
+    "Fandom",
+    "Merchandise",
+    "Voice Actors",
+    "Art Styles",
+    "Directors",
+    "Symbolism",
+    "Mecha",
+    "Romance",
+    "Fantasy Worlds",
+    "Slice of Life",
+    "Strong Female Characters",
+    "Antiheroes",
+    "Villains",
+    "Comedy",
+    "Fan Art"
 ]
 
 # letters
@@ -53,26 +53,25 @@ alphabet = [
     'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
 ]
 
-def many(num):
+def demo():
     """
-    how many catgories or letter do u want?
+    a demo to show how it works
     """
-    while True:
-        try:
-            print("\nenter a number for categories (and letter for hard mode)")
-            num = int(input("So how many?:  "))
-            if num > len(categories) or num > len(alphabet):
-                print("how many categoires do you want???\n")
-            elif num <= 0:
-                print("haha I see what you did there.")
-                print("please enter a positive number and not 0 either\n")
-            elif num > 0 and num < len(categories) and num < len(alphabet):
-                break
-            
-        except ValueError:
-            print("I thought you are better than this")
-            print("Please enter a number\n\n")
-    return num
+    print("***** Welocome to Anime Scattegories! *****")
+    print("the rules are simple:")
+    print("* Pick a mode, E for easy and H for hard (Or press X to exit)")
+    print("* Easy gives you one letter for all the catgories")
+    print("* Hard gives you a letter per category")
+    print("* In both modes you can choose the ammount of categories")
+    print("\n\n***** Now for the actual game *****")
+    print("- work in groups or alone")
+    print("- any double ups will result in 0 points")
+    print("  for the groups involved for that category")
+    print("- if a group presents an answer with 2 words both starting")
+    print("  with the rounds letter, give double points")
+    print("- the team with the most points as the end of the round wins")
+    print("- idk how to deal with ties you decide what to do :)")
+    
     
 
 def scattegories():
@@ -80,6 +79,7 @@ def scattegories():
     basically the main routine, asks for the mode
     and then directs you to correct function
     """
+    
     # shuffle lists
     random.shuffle(categories)
     random.shuffle(alphabet)
@@ -97,6 +97,28 @@ def scattegories():
             exit()
         else:
             print("enter something valid pls")
+
+def many(num):
+    """
+    how many catgories or letter do u want?
+    """
+    
+    while True:
+        try:
+            print("\nenter a number for categories (and letter for hard mode)")
+            num = int(input("So how many?:  "))
+            if num > len(categories) or num > len(alphabet):
+                print("how many categoires do you want???\n")
+            elif num <= 0:
+                print("haha I see what you did there.")
+                print("please enter a positive number and not 0 either\n")
+            elif num > 0 and num < len(categories) and num < len(alphabet):
+                break
+            
+        except ValueError:
+            print("I thought you are better than this")
+            print("Please enter a number\n\n")
+    return num
             
 
     
@@ -147,6 +169,7 @@ def hard():
 # mains routine
 if __name__ == '__main__':
 
+    demo()
     # forever loop
     while True:
         scattegories()
